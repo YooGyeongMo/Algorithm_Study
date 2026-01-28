6403cfad80573e1a487ba2843f4f11e11c15d067
Demian YOO (유경모) <98875580+YooGyeongMo@users.noreply.github.com>
2026-01-28 12:24:13 +0900

    [Bronze I] Title: 이항 계수 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- C++17/백준/Bronze/11050. 이항 계수 1/이항 계수 1.cc
+++ C++17/백준/Bronze/11050. 이항 계수 1/이항 계수 1.cc
@@ -2,28 +2,27 @@
 
 using namespace std;
 
+int factorial(int n) {
+    int result = 1;
+
+    for (int i = 1; i<=n; i++) {
+        result *= i;
+    }
+
+    return result;
+}
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    //nCk
+    int N, K;
 
-    int N,K;
-    int arr[11][11] = {0};
     cin >> N >> K;
 
-    for (int i = 0; i<11; i++) {
-        arr[i][i] = 1;
-        arr[i][0] = 1;
-    }
+    int result = factorial(N) / (factorial(K) * factorial(N-K));
 
-    for (int i = 2; i<=N; i++) {
-        for (int j = 1; j<=N; j++ ) {
-            arr[i][j] = arr[i-1][j-1] + arr[i-1][j];
-        }
-    }
-
-    cout << arr[N][K];
+    cout << result ;
 
     return 0;
 }
