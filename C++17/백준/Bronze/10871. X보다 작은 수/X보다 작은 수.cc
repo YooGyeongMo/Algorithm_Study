fc5b59fba980b0ed60667754e8bbae7f5cbb5629
Demian YOO (유경모) <98875580+YooGyeongMo@users.noreply.github.com>
2026-03-06 14:03:10 +0900

    [Bronze V] Title: X보다 작은 수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- C++17/백준/Bronze/10871. X보다 작은 수/X보다 작은 수.cc
+++ C++17/백준/Bronze/10871. X보다 작은 수/X보다 작은 수.cc
@@ -2,21 +2,24 @@
 
 using namespace std;
 
-int main() {
-    ios_base::sync_with_stdio(false);
+int main(void) {
+    ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int N, X;
-    int arr[10000];
+    int N, X, a[10001];
+
     cin >> N >> X;
 
-    for (int i = 0; i < N; i++) {
-        cin >> arr[i];
+    for (int i = 0; i<N; i++) {
+        cin >> a[i];
     }
 
-    for (int i = 0; i < N; i++ ) {
-        if (X > arr[i]) {
-            cout << arr[i] << " ";
+    for (int i = 0; i<N; i++) {
+        if (a[i] < X) {
+            cout << a[i] <<' ';
         }
     }
+
+
+    return 0;
 }
