b7d7ad384ea82dcd41699e0b6ccbd1d5c40a472a
Demian YOO (유경모) <98875580+YooGyeongMo@users.noreply.github.com>
2026-04-02 12:47:23 +0900

    [Silver II] Title: 에디터, Time: 32 ms, Memory: 11088 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.cc
+++ 백준/Silver/1406. 에디터/에디터.cc
@@ -4,6 +4,7 @@
 using namespace std;
 
 const int MX = 1000005;
+
 char dat[MX];
 int pre[MX], nxt[MX];
 int unused = 1;
@@ -14,29 +15,32 @@ void insert(int addr, char chr) {
     nxt[unused] = nxt[addr];
 
     if (nxt[addr] != -1) pre[nxt[addr]] = unused;
+
     nxt[addr] = unused;
     unused++;
 }
 
 void erase(int addr) {
     nxt[pre[addr]] = nxt[addr];
-    if (nxt[addr] != -1) pre[nxt[addr]] = pre[addr];
+    if (nxt[addr] != - 1) pre[nxt[addr]] = pre[addr];
 }
 
 void traverse() {
     int cur = nxt[0];
+
     while (cur != -1) {
         cout << dat[cur];
         cur = nxt[cur];
     }
+
+    // for (int cur = nxt[0]; cur != -1; cur = nxt[cur]) {
+    //     cout << dat[cur];
+    // }
 }
 
 int main(void) {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
-
-    fill(pre,pre+MX,-1);
-    fill(nxt,nxt+MX, -1);
 
     string str;
     int N;
@@ -44,8 +48,11 @@ int main(void) {
 
     cin >> str >> N;
 
-    for (char c: str) {
-        insert(cursor,c);
+    fill(pre,pre+MX,-1);
+    fill(nxt,nxt+MX,-1);
+
+    for (char c : str) {
+        insert(cursor, c);
         cursor++;
     }
 
@@ -53,11 +60,11 @@ int main(void) {
         char cmp;
         cin >> cmp;
 
-        if ( cmp == 'L') {
-            if (pre[cursor] != -1) cursor = pre[cursor];
+        if (cmp == 'L') {
+            if (pre[cursor] != - 1) cursor = pre[cursor];
         }
         else if (cmp == 'D') {
-            if (nxt[cursor] != -1 ) cursor = nxt[cursor];
+            if (nxt[cursor] != -1) cursor = nxt[cursor];
         }
         else if (cmp == 'B') {
             if (pre[cursor] != -1) {
@@ -66,13 +73,11 @@ int main(void) {
             }
         }
         else {
-            char x ;
+            char x;
             cin >> x;
             insert(cursor, x);
             cursor = nxt[cursor];
         }
     }
-
     traverse();
-
 }
