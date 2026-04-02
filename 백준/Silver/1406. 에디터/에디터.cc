7a85284fa11fb1c717ed19e157556a6bfcf4b06a
Demian YOO (유경모) <98875580+YooGyeongMo@users.noreply.github.com>
2026-04-02 11:45:49 +0900

    [Silver II] Title: 에디터, Time: 56 ms, Memory: 21020 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.cc
+++ 백준/Silver/1406. 에디터/에디터.cc
@@ -1,78 +1,51 @@
 #include <iostream>
 #include <string>
+#include <list>
 
 using namespace std;
-
-const int MX = 1000005;
-char dat[MX];
-int pre[MX];
-int nxt[MX];
-int unused = 1;
-
-void insert (int addr, char chr) {
-    dat[unused] = chr;
-    pre[unused] = addr;
-    // 처음에 넣을때는 지금 내가 마지막이면 -1을 가리키는게 맞음.
-    nxt[unused] = nxt[addr];
-    // nxt[addr]은 지금 추가하려는 것의 다음의 dat의 주소
-    if (nxt[addr] != -1) pre[nxt[addr]] = unused;
-    // 전에꺼 다음을 가리킴 cursor
-    nxt[addr] = unused;
-    unused++;
-}
-
-void erase( int addr ) {
-    nxt[pre[addr]] = nxt[addr];
-    if (nxt[addr] != -1) pre[nxt[addr]] = pre[addr];
-}
-
-void traverse() {
-    int cur = nxt[0];
-    while (cur != -1) {
-        cout << dat[cur];
-        cur = nxt[cur];
-    }
-}
 
 int main(void) {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    fill(pre,pre+MX, -1);
-    fill(nxt,nxt+MX, -1);
-
     string str;
     int N;
-    int cursor = 0;
+    list<char> str2;
     cin >> str >> N;
 
     for (char c : str) {
-        insert(cursor, c);
-        cursor++;
+        str2.push_back(c);
     }
-    
-    while (N--) {
-        char op;
-        cin >> op;
-        if (op == 'L') {
-            if (pre[cursor] != -1) cursor = pre[cursor];
+    list<char> :: iterator cursor = str2.end();
+
+    for (int i = 0; i<N; i++) {
+        char cmp;
+        cin >> cmp;
+        if (cmp == 'P') {
+            char x;
+            cin >> x;
+            str2.insert(cursor, x);
         }
-        else if (op == 'D') {
-            if (nxt[cursor] != -1) cursor = nxt[cursor];
+        else if (cmp == 'L') {
+            if (cursor != str2.begin()) {
+                cursor--;
+            }
         }
-        else if (op == 'B') {
-            if (pre[cursor] != -1) {
-                erase(cursor);
-                cursor = pre[cursor];
+        else if ( cmp == 'D') {
+            if (cursor != str2.end()) {
+                cursor++;
             }
         }
         else {
-            char x;
-            cin >> x;
-            insert(cursor, x);
-            cursor = nxt[cursor];
+            if (cursor != str2.begin()) {
+                cursor--;
+                cursor = str2.erase(cursor);
+            }
         }
     }
 
-    traverse();
+    for (char c: str2) {
+        cout << c;
+    }
+
 }
