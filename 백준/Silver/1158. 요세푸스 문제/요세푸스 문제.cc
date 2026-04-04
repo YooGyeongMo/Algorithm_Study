d91c180c39041f59887eb7cf93cebda021f2c380
Demian YOO (유경모) <98875580+YooGyeongMo@users.noreply.github.com>
2026-04-05 00:13:47 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 36 ms, Memory: 2196 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.cc
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.cc
@@ -1,34 +1,46 @@
 #include <iostream>
-#include <deque>
+#include <vector>
 
 using namespace std;
 
+int pre[5001];
+int nxt[5001];
+
+vector <int> v;
 int main(void) {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    deque<int> queue = {};
-
-    int N, K;
+    int N, K, len = 0;
 
     cin >> N >> K;
 
     for (int i = 1; i<=N; i++) {
-        queue.push_back(i);
+        pre[i] = (i==1) ? N : i-1;
+        nxt[i] = (i==N) ? 1 : i+1;
+        len++;
     }
-    int idx = 0;
+
+    int idx = 1;
+
+    for (int cur = 1; len != 0; cur = nxt[cur]) {
+        if (idx == K) {
+            pre[nxt[cur]] = pre[cur];
+            nxt[pre[cur]] = nxt[cur];
+            v.push_back(cur);
+            idx = 1;
+            len--;
+        } else idx++;
+    }
+
+
 
     cout << '<';
 
-    while (!queue.empty()) {
-        idx = (idx + K - 1) % queue.size();
-
-        cout << queue[idx];
-        queue.erase(queue.begin()+idx);
-
-        if (!queue.empty()) {
-            cout << ", ";
-        }
+    for (int i = 0 ; i<v.size(); i++) {
+        cout << v[i];
+        if (i != v.size() - 1) cout <<", ";
     }
+
     cout << '>';
 }
