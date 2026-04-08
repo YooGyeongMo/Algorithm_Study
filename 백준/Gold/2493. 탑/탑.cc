818d0dee988d2ef404592b696120fcf25106926b
Demian YOO (유경모) <98875580+YooGyeongMo@users.noreply.github.com>
2026-04-09 00:58:48 +0900

    [Gold V] Title: 탑, Time: 92 ms, Memory: 6300 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.cc
+++ 백준/Gold/2493. 탑/탑.cc
@@ -1,34 +1,26 @@
-#include <iostream>
-#include <stack>
-#include <utility>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int main(void) {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
-
     int N;
     cin >> N;
 
-    stack<pair<int, int>> tower; // {높이, 번호}
+    stack<pair<int,int>> tower;
 
-    for (int i = 1; i <= N; i++) {
+    for (int i = 0; i < N; i++) {
         int height;
         cin >> height;
 
         while (!tower.empty() && tower.top().first < height) {
             tower.pop();
         }
+        if (tower.empty()) cout << 0 << " ";
+        else cout << tower.top().second << " ";
 
-        if (tower.empty()) {
-            cout << 0 << " ";
-        } else {
-            cout << tower.top().second << " ";
-        }
+        tower.push({height,i+1});
 
-        tower.push({height, i});
     }
-
-    return 0;
 }
