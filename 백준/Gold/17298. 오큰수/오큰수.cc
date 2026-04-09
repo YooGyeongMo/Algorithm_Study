889fee777f5205e86932c1132f1a051bde742724
Demian YOO (유경모) <98875580+YooGyeongMo@users.noreply.github.com>
2026-04-09 14:50:54 +0900

    [Gold IV] Title: 오큰수, Time: 184 ms, Memory: 12496 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.cc
+++ 백준/Gold/17298. 오큰수/오큰수.cc
@@ -4,14 +4,12 @@
 using namespace std;
 
 int a[1000001] = {};
-int ans[1000001];
+int ans[1000001] = {};
 stack<int> s;
 
 int main(void) {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
-
-    fill(ans, ans+1000001, -1);
 
     int N;
     cin >> N;
@@ -20,12 +18,14 @@ int main(void) {
         cin >> a[i];
     }
 
-    for (int i = 0; i<N; i++) {
-        while ( !s.empty() && a[s.top()] < a[i] ) {
-            ans[s.top()] = a[i];
+    for (int i = N-1; i>-1; i--) {
+        while ( !s.empty() && s.top() <= a[i] ) {
             s.pop();
         }
-        s.push(i);
+        if (s.empty()) ans[i] = -1;
+        else ans[i] = s.top();
+        
+        s.push(a[i]);
     }
 
     for (int i = 0; i<N; i++) {
