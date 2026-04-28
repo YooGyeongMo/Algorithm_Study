66e34b6c2b2462f000d6da9af1225a44691e87a6
Demian YOO (유경모) <98875580+YooGyeongMo@users.noreply.github.com>
2026-04-29 02:20:05 +0900

    [level 1] Title: 약수의 합, Time: 2.53 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.swift
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.swift
@@ -1,16 +1,8 @@
 func solution(_ n:Int) -> Int {
     
-    var result = 0
-    
-    if n == 0 {
-        return 0
+    guard n != 0 else {
+        return 0   
     }
     
-    for i in 1...n {
-        if n%i == 0 {
-            result += i
-        }
-    }
-    
-    return result
+    return Array(1...n).filter{n % $0 == 0}.reduce(0,+)
 }
