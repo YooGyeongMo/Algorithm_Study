116f250eeae6435b0389b57022121c3fab513c08
Demian YOO (유경모) <98875580+YooGyeongMo@users.noreply.github.com>
2026-04-29 21:26:09 +0900

    [level 1] Title: 자연수 뒤집어 배열로 만들기, Time: 0.06 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.swift
+++ 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.swift
@@ -2,17 +2,5 @@ import Foundation
 
 func solution(_ n:Int64) -> [Int] {
     
-    var result: [Int] = []
-    var s = Int(n)
-    
-    while s > 0 {
-        var digit = 0
-        
-        digit = s%10
-        s /= 10
-        
-        result.append(digit)
-    }
-    
-    return result
+    return "\(n)".compactMap{ $0.wholeNumberValue! }.reversed()
 }
