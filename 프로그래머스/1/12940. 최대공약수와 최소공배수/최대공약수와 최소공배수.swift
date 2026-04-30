ebb00597467be4d7081a8dc970347d95532dfeac
Demian YOO (유경모) <98875580+YooGyeongMo@users.noreply.github.com>
2026-05-01 01:09:36 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.02 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.swift
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.swift
@@ -1,11 +1,9 @@
 func solution(_ n:Int, _ m:Int) -> [Int] {
-    
     func gcd(_ a: Int, _ b: Int) -> Int {
-        return b == 0 ? a : gcd(b,a%b)
+        return b == 0 ? a : gcd(b,(a%b))
     }
     
-    var a = gcd(n,m)
-    var b = (n*m)/a
+    let gcd2 = gcd(n,m)
     
-    return [a,b]
+    return [gcd2, (n*m)/gcd2]
 }
