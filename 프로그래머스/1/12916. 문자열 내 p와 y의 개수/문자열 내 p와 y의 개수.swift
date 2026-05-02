9530d111725402c6cdfd0dc53f37a7d1944e7411
Demian YOO (유경모) <98875580+YooGyeongMo@users.noreply.github.com>
2026-05-03 00:47:45 +0900

    [level 1] Title: 문자열 내 p와 y의 개수, Time: 0.10 ms, Memory: 16.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+func solution(_ s:String) -> Bool
+{
+    
+    let str = s.lowercased()
+    
+    var pCount = str.filter{ $0 == "p"}.count
+    var yCount = str.filter{ $0 == "y"}.count
+    
+    return pCount == yCount
+}
