6586307d398caff09c798c1782b4ab9977e75a36
Demian YOO (유경모) <98875580+YooGyeongMo@users.noreply.github.com>
2026-05-06 01:44:28 +0900

    [level 2] Title: 기능개발, Time: 0.04 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.swift
+++ 프로그래머스/2/42586. 기능개발/기능개발.swift
@@ -2,22 +2,29 @@ import Foundation
 
 func solution(_ progresses:[Int], _ speeds:[Int]) -> [Int] {
     
-    var release: [Int] = []
+    var days: [Int] = []
     var previousDay = 0
     
     for i in 0..<progresses.count {
         let progress = progresses[i]
         let speed = speeds[i]
         let releaseDay = Int(ceil(Double(100-progress) / Double(speed)))
-        
-        if previousDay >= releaseDay {
-            release[release.count-1] += 1
-        }
-        else {
-            release.append(1)
-            previousDay = releaseDay
-        }
+        days.append(releaseDay)
     }
     
-    return release
+    var result: [Int] = []
+    var front = 0
+    
+    while front < days.count {
+        let deployDay = days[front]
+        var count = 0
+        
+        while front < days.count && days[front] <= deployDay {
+            front += 1
+            count += 1
+        }
+        result.append(count)
+    }
+    
+    return result
 }
