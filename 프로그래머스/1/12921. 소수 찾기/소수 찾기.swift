3aafd7ae754f3ed42966ad4fd3ea17d135ca0c96
Demian YOO (유경모) <98875580+YooGyeongMo@users.noreply.github.com>
2026-05-06 01:01:18 +0900

    [level 1] Title: 소수 찾기, Time: 287.99 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.swift
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.swift
@@ -1,29 +1,18 @@
 func solution(_ n:Int) -> Int {
-    if n < 2 { return 0 }
-    
-    var isPrime = Array(repeating: 0 , count: n+1)
-    var cnt = 0 
-    
-    isPrime[0] = 1
-    isPrime[1] = 1
+    var isPrime = Array(repeating: true, count: n+1)
+    isPrime[0] = false
+    isPrime[1] = false
     
     var i = 2
     
     while i*i <= n {
-        if isPrime[i] == 0 {
-            for j in stride(from: i*i, to: n+1, by: i) {
-                isPrime[j] = 1
+        if isPrime[i] {
+            for j in stride(from: i*i, through: n, by: i) {
+                isPrime[j] = false
             }
         }
-        
         i += 1
     }
     
-    for j in 0...n {
-        if isPrime[j] == 0 {
-            cnt += 1
-        }
-    }
-    
-    return cnt
+    return isPrime.filter{ $0 }.count
 }
