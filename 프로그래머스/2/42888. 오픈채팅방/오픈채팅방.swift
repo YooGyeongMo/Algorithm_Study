feebd2f2a556f79cfc8172d1cf0c24ac7890e016
Demian YOO (유경모) <98875580+YooGyeongMo@users.noreply.github.com>
2026-05-05 23:06:13 +0900

    [level 2] Title: 오픈채팅방, Time: 475.49 ms, Memory: 54.1 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.swift
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.swift
@@ -2,42 +2,40 @@ import Foundation
 
 func solution(_ record: [String]) -> [String] {
     
-    var nickNameDict: [String: String] = [:]
-    var log: [(command: String, uid: String)] = []
-    
-    for item in record {
-        let part = item.split(separator: " ").map{String($0)}
-        
-        let cmd = part[0]
-        let uid = part[1]
-        
-        if cmd == "Enter" {
-            let nickName = part[2]
-            nickNameDict[uid] = nickName
-            log.append((cmd, uid))
-        }
-        else if cmd == "Leave" {
-            log.append((cmd, uid))
-        }
-        else {
-            let nickName = part[2]
-            nickNameDict[uid] = nickName
-        }
-    }
+    var nickName: [String: String] = [:]
+    var logs: [(command: String, id: String)] = []
     
     var result: [String] = []
     
-    for item in log {
-        let name = nickNameDict[item.uid]!
+    for item in record {
+        let part = item.split(separator: " ").map{ String($0) }
         
-        if item.command == "Enter" {
-            result.append("\(name)님이 들어왔습니다.")
+        let cmd = part[0]
+        let uID = part[1]
+        
+        if cmd == "Enter" {
+            let name = part[2]
+            nickName[uID] = name
+            logs.append((cmd, uID))
+        }
+        else if cmd == "Leave" {
+            logs.append((cmd, uID))
         }
         else {
-            result.append("\(name)님이 나갔습니다.")
+            let name = part[2]
+            nickName[uID] = name
         }
     }
     
+    for item in logs {
+        
+        if item.command == "Enter" {
+            result.append("\(nickName[item.id]!)님이 들어왔습니다.")
+        }
+        else {
+            result.append("\(nickName[item.id]!)님이 나갔습니다.")
+        }
+    }
     
     return result
 }
